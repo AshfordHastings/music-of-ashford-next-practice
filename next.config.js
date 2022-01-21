c4d0ba6dc5c1e661fc9711cec53f8103ed30878f
AshfordHastings <ashfordh1@gmail.com>
2022-01-21 09:47:31 -0600

    updated homepage redirect function

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/albumLists',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
   env: {
     SPOTIFY_REFRESH_TOKEN: 'AQB-x4IzyTVpF4AZo1ZkDrJQEEt5YiDsHfzg0xXso3FdLHJSOBv9vTv0dkPWkVPka8uQCGGo-TwrNVyo_UUxsmjS3OgkuljVKsUtEF_BVnfz_2oUdhNXIaTkv7VbJXYLBo4'
